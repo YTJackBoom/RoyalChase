eabe0607924a95713419a46fa229b06afcc7b09d
janne <janne@DESKTOP-CG8NDC7.fritz.box>
2023-05-18 00:52:06 +0200

    Fixed Map, tower need work still

--- src/basics/Map.java
+++ src/basics/Map.java
@@ -64,7 +64,9 @@ public class Map extends JPanel {
 	
 	@Override
 	protected void paintComponent(Graphics g) {
-
+		super.paintComponent(g);
+		
+		
 		Graphics2D g2d = (Graphics2D) g;	
 		try {
 			mapImage = ImageIO.read(getMapFile());
